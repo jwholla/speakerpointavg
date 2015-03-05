a81440ad630fb11ab17d46a4c02b9c23b1fc001b
jwholla <johnholland00@gmail.com>
2015-03-04 21:00:45 -0500

    added average points and sorted to sheet

--- SpeakerPoints2015.py
+++ SpeakerPoints2015.py
@@ -53,21 +53,13 @@ combined_entries_txt = open(combined_entries, 'r+')
 
 ###print values at the end
 speaks= os.path.join(dir,'output\speaker_points.txt')
-
+speaks_avg= os.path.join(dir,'output\speaker_average.txt')
 speaker_points= open(speaks, 'r+')
 speaker_points.truncate()
+speaker_avg=open(speaks_avg, 'r+')
+speaker_avg.truncate()
 key_array=[]
-#for key, value in ndt_points.iteritems() :
-#	speaker_points.write(key)
-#	speaker_points.write(' ')
-#	k=0
-#	while k < len(value):
-#		temp=str(value[k])
-#		speaker_points.write(temp)
-#		speaker_points.write(' ')
-#		k+=1
-#	speaker_points.write('\n')
-#print sorted list
+
 for key, value in ndt_points.iteritems() :
 	key_array.append(key)
 	key_array.sort()
@@ -84,10 +76,56 @@ for i in key_array:
 		k+=1
 	speaker_points.write('\n')
 speaker_points.close()
+ndt_average={}
 
 #ndt_points contains a dictionary with a list
+#for each entry in the dictionary, i want to create an entry in a new dictionary that contains that average points
+for key, value in ndt_points.iteritems():
+	#calculate total average
+	i=0
+	running_sum=0
+	running_count=0
+	if len(value) >= 1:
+		while i < len(value):
+			#value[0] is GSU
+			counter1=float(value[i+1])
+			#print counter1
+			#value[1] is 16
+			counter2=float(value[i+2])
+			#print counter2
+			#value[2] is average from GSU
+			counter3=counter1*counter2
+			#print counter3
+			running_sum=running_sum+counter3
+			running_count=running_count+counter1
+			#the order is tourn_name, num_rounds, average
+			#take value[1], cast as float
+			#create a sum of values
+			#take value[2], cast as float
+			#multiply
+			#create a sum of these values
+			#divide at end
+			#this gives average
+			i+=3
+			#print running_count
+		#print running_count
+		average=running_sum/running_count
+		ndt_average.setdefault(key,[]).append(average)
+	else:
+		ndt_average.setdefault(key,[]).append('No tournaments attended')
 
-#for key, value in ndt_points.iteritems():
-#	print key, value
+key_array2=[]
+for key, value in ndt_average.iteritems() :
+	key_array2.append(key)
+	key_array2.sort()
+
+for i in key_array2:
+	speaker_avg.write(i)
+	speaker_avg.write(' ')
+	speaker_avg.write(str(ndt_average[i]))
+	speaker_avg.write('\n')
+speaker_avg.close()
+for key, value in ndt_average.iteritems():
+	print key, value
 
 input("Press Enter to continue...")
