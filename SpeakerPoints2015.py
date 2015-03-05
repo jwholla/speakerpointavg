3dc24d3486da0da0646092cd46bc898ae0722acc
jwholla <johnholland00@gmail.com>
2015-03-04 19:34:15 -0500

    created new file for validation methods

--- SpeakerPoints2015.py
+++ SpeakerPoints2015.py
@@ -1,12 +1,12 @@
 import xml.etree.ElementTree as ET
 import sys
 import os
+from validation import b, c, initials_check, calculate_points, get_team_name
 
 dir = os.path.dirname(__file__)
 ndt_data = os.path.join(dir, 'results\\ndt_data_3_3.xml')
 ndt_entries = os.path.join(dir, 'entries\\ndt_entries.txt')
 gsu_results= os.path.join(dir, 'results\\gsu_results_3_3.xml')
-gsu_entries= os.path.join(dir,'entries\\gsu_entries.txt')
 
 #creates a tree of the team list for the NDT
 ndt = ET.parse(ndt_data)
@@ -33,7 +33,6 @@ ndt_entries_txt.close()
 
 gsu=ET.parse(gsu_results)
 gsu_root=gsu.getroot()
-gsu_entries_txt = open(gsu_entries, 'r+')
 gsu_codes={} #this dictionary takes their id in tabroom and corresponds to team code
 gsu_points={} #this corresponds team id to points
 
@@ -41,22 +40,13 @@ for entry in gsu_root.findall('ENTRY'):
 	code = entry.find('CODE').text #Emory KS
 	id=entry.find('ID').text #team ID
 	#check both initials
-	initials=code[-2:]
-	code_reversed=code[:-2]+initials[1]+initials[0]
-	#if NDT team then do things
-	if code in ndt_entries_list:
-	# or (code_reversed in ndt_entries_list)
-		gsu_entries_txt.write(code)
-		gsu_entries_txt.write('\n')
-	#I now have all of the team codes
+	code_reversed= initials_check(code)
+	if code in ndt_entries_list: 	#if NDT team then do things
 		gsu_entries_list.append(code)
 		gsu_codes.setdefault(id, []).append(code)
 		gsu_codes.setdefault(id, []).append('GSU')
 		gsu_points.setdefault(id,[])
 	elif code_reversed in ndt_entries_list:
-		gsu_entries_txt.write(code_reversed)
-		gsu_entries_txt.write('\n')
-	#I now have all of the team codes
 		gsu_entries_list.append(code)
 		gsu_codes.setdefault(id, []).append(code_reversed)
 		gsu_codes.setdefault(id, []).append('GSU')
@@ -69,13 +59,12 @@ for entry_student in gsu_root.findall('ENTRY_STUDENT'):
 	entry=entry_student.find('ENTRY').text #team ID
 	if entry in gsu_codes:
 		gsu_students.setdefault(id, []).append(entry)
-	#now the student is the primary key
+	#student is the primary key
 	
 for ballots in gsu_root.findall('BALLOT_SCORE'):
 	score_id=ballots.find('SCORE_ID').text #points or not
 	score=ballots.find('SCORE').text #speaker points
 	debater=ballots.find('RECIPIENT').text #something
-	
 	if score_id=='POINTS':
 		if debater in gsu_students:
 			#look up team id
@@ -89,33 +78,19 @@ for ballots in gsu_root.findall('BALLOT_SCORE'):
 for key, value in gsu_points.iteritems():
 	#value is a list of points that need to be converted
 	#key is the id that we append to when we're done
-	j=0
-	test=value
-	for i in value:
-		test[j]= float(i)
-		j+=1
-	total = sum(test)/len(test)
+	total=calculate_points(value)
 	gsu_codes.setdefault(key,[]).append(total)
-	key2=gsu_codes[key]
-	key3=key2[0]
+	team_name=get_team_name(gsu_codes,key)
 	###this is where i can append to the ndt points one
-	ndt_points.setdefault(key3,[]).append('GSU')
-	ndt_points.setdefault(key3,[]).append(total)
+	ndt_points.setdefault(team_name,[]).append('GSU')
+	ndt_points.setdefault(team_name,[]).append(total)
 	#	gsu_points.setdefault(value[0], []).append(score)
 
 #build a table that links code to ID
 
-#for key, value in gsu_codes.iteritems() :
-#	print key, value
-#for key, value in gsu_students.iteritems():
-#	print key, value
-#for key, value in gsu_points.iteritems():
-#	print key, value
-gsu_entries_txt.close()
 combined_entries= os.path.join(dir,'entries\combined_entries.txt')
 
 combined_entries_txt = open(combined_entries, 'r+')
-#need to make a change here to account for reverse partnerships
 
 
 #########Kentucky
@@ -132,22 +107,15 @@ for entry in uk_root.findall('ENTRY'):
 	code = entry.find('CODE').text #Emory KS
 	id=entry.find('ID').text #team ID
 	#check both initials
-	initials=code[-2:]
-	code_reversed=code[:-2]+initials[1]+initials[0]
+	code_reversed= initials_check(code)
 	#if NDT team then do things
 	if code in ndt_entries_list:
-	# or (code_reversed in ndt_entries_list)
-		uk_entries_txt.write(code)
-		uk_entries_txt.write('\n')
 	#I now have all of the team codes
 		uk_entries_list.append(code)
 		uk_codes.setdefault(id, []).append(code)
 		uk_codes.setdefault(id, []).append('UK')
 		uk_points.setdefault(id,[])
 	elif code_reversed in ndt_entries_list:
-		uk_entries_txt.write(code_reversed)
-		uk_entries_txt.write('\n')
-	#I now have all of the team codes
 		uk_entries_list.append(code)
 		uk_codes.setdefault(id, []).append(code_reversed)
 		uk_codes.setdefault(id, []).append('UK')
@@ -172,26 +140,18 @@ for ballots in uk_root.findall('BALLOT_SCORE'):
 		if debater in uk_students:
 			#look up team id
 			value=uk_students[debater]
-			#tel['jack'] returns the value, which means i need the team id as the key
 			uk_points.setdefault(value[0], []).append(score)
 for key, value in uk_points.iteritems():
 	#value is a list of points that need to be converted
 	#key is the id that we append to when we're done
-	j=0
-	test=value
-	for i in value:
-		test[j]= float(i)
-		j+=1
-	total = sum(test)/len(test)
+	total=calculate_points(value)
 	uk_codes.setdefault(key,[]).append(total)
-	key2=uk_codes[key]
-	key3=key2[0]
+	team_name=get_team_name(uk_codes,key)
 	###this is where i can append to the ndt points one
-	ndt_points.setdefault(key3,[]).append('UK')
-	ndt_points.setdefault(key3,[]).append(total)
+	ndt_points.setdefault(team_name,[]).append('UK')
+	ndt_points.setdefault(team_name,[]).append(total)
 #merge UK and GSU results
-#for key, value in uk_codes.iteritems():
-#	gsu_codes.setdefault(key,[]).append(value)
+
 
 #####add umkc
 #uk_entries= os.path.join(dir,'entries\combined_entries.txt')
@@ -199,7 +159,6 @@ umkc_results= os.path.join(dir,'results\umkc_results_3_4.xml')
 
 umkc=ET.parse(umkc_results)
 umkc_root=umkc.getroot()
-#umkc_entries_txt = open(umkc_entries, 'r+')
 umkc_entries_list=[]
 umkc_codes={}
 umkc_points={}
@@ -207,29 +166,20 @@ for entry in umkc_root.findall('ENTRY'):
 	code = entry.find('CODE').text #Emory KS
 	id=entry.find('ID').text #team ID
 	#check both initials
-	initials=code[-2:]
-	code_reversed=code[:-2]+initials[1]+initials[0]
+	code_reversed=initials_check(code)
 	#if NDT team then do things
 	if code in ndt_entries_list:
-	# or (code_reversed in ndt_entries_list)
-#		umkc_entries_txt.write(code)
-#		umkc_entries_txt.write('\n')
-	#I now have all of the team codes
 		umkc_entries_list.append(code)
 		umkc_codes.setdefault(id, []).append(code)
 		umkc_codes.setdefault(id, []).append('umkc')
 		umkc_points.setdefault(id,[])
 	elif code_reversed in ndt_entries_list:
-#		umkc_entries_txt.write(code_reversed)
-#		umkc_entries_txt.write('\n')
-	#I now have all of the team codes
 		umkc_entries_list.append(code)
 		umkc_codes.setdefault(id, []).append(code_reversed)
 		umkc_codes.setdefault(id, []).append('umkc')
 		umkc_points.setdefault(id,[])
 #primary key is now the ID
 
-#umkc_entries_txt.close()
 umkc_students={} #this corresponds to their team id in tabroom and the debater id
 for entry_student in umkc_root.findall('ENTRY_STUDENT'):
 	id=entry_student.find('ID').text #student ID
@@ -247,23 +197,16 @@ for ballots in umkc_root.findall('BALLOT_SCORE'):
 		if debater in umkc_students:
 			#look up team id
 			value=umkc_students[debater]
-			#tel['jack'] returns the value, which means i need the team id as the key
 			umkc_points.setdefault(value[0], []).append(score)
 for key, value in umkc_points.iteritems():
 	#value is a list of points that need to be converted
 	#key is the id that we append to when we're done
-	j=0
-	test=value
-	for i in value:
-		test[j]= float(i)
-		j+=1
-	total = sum(test)/len(test)
+	total=calculate_points(value)
 	umkc_codes.setdefault(key,[]).append(total)
-	key2=umkc_codes[key]
-	key3=key2[0]
+	team_name=get_team_name(umkc_codes,key)
 	###this is where i can append to the ndt points one
-	ndt_points.setdefault(key3,[]).append('UMKC')
-	ndt_points.setdefault(key3,[]).append(total)
+	ndt_points.setdefault(team_name,[]).append('UMKC')
+	ndt_points.setdefault(team_name,[]).append(total)
 #merge umkc
 
 ####add Harvard
@@ -280,21 +223,14 @@ for entry in harvard_root.findall('ENTRY'):
 	code = entry.find('CODE').text #Emory KS
 	id=entry.find('ID').text #team ID
 	#check both initials
-	initials=code[-2:]
-	code_reversed=code[:-2]+initials[1]+initials[0]
+	code_reversed=initials_check(code)
 	#if NDT team then do things
 	if code in ndt_entries_list:
-	# or (code_reversed in ndt_entries_list)
-#		umkc_entries_txt.write(code)
-#		umkc_entries_txt.write('\n')
-	#I now have all of the team codes
 		harvard_entries_list.append(code)
 		harvard_codes.setdefault(id, []).append(code)
 		harvard_codes.setdefault(id, []).append('harvard')
 		harvard_points.setdefault(id,[])
 	elif code_reversed in ndt_entries_list:
-#		umkc_entries_txt.write(code_reversed)
-#		umkc_entries_txt.write('\n')
 	#I now have all of the team codes
 		harvard_entries_list.append(code)
 		harvard_codes.setdefault(id, []).append(code_reversed)
@@ -310,33 +246,28 @@ for entry_student in harvard_root.findall('ENTRY_STUDENT'):
 	if entry in harvard_codes:
 		harvard_students.setdefault(id, []).append(entry)
 	#now the student is the primary key
-	
+temp_score=0.0
 for ballots in harvard_root.findall('BALLOT_SCORE'):
 	score_id=ballots.find('SCORE_ID').text #points or not
+	temp_score=float(ballots.find('SCORE').text)
 	score=ballots.find('SCORE').text #speaker points
 	debater=ballots.find('RECIPIENT').text #something
+	if temp_score > 25:
 	
-	if score_id=='POINTS':
-		if debater in harvard_students:
-			#look up team id
-			value=harvard_students[debater]
-			#tel['jack'] returns the value, which means i need the team id as the key
-			harvard_points.setdefault(value[0], []).append(score)
+		if score_id=='POINTS':
+			if debater in harvard_students:
+				#look up team id
+				value=harvard_students[debater]
+				harvard_points.setdefault(value[0], []).append(score)
 for key, value in harvard_points.iteritems():
 	#value is a list of points that need to be converted
 	#key is the id that we append to when we're done
-	j=0
-	test=value
-	for i in value:
-		test[j]= float(i)
-		j+=1
-	total = sum(test)/len(test)
+	total=calculate_points(value)
 	harvard_codes.setdefault(key,[]).append(total)
-	key2=harvard_codes[key]
-	key3=key2[0]
+	team_name=get_team_name(harvard_codes,key)
 	###this is where i can append to the ndt points one
-	ndt_points.setdefault(key3,[]).append('harvard')
-	ndt_points.setdefault(key3,[]).append(total)
+	ndt_points.setdefault(team_name,[]).append('harvard')
+	ndt_points.setdefault(team_name,[]).append(total)
 #merge harvard
 
 
@@ -356,8 +287,6 @@ for key, value in ndt_points.iteritems() :
 		k+=1
 	speaker_points.write('\n')
 speaker_points.close()
-#for key, value in uk_codes.iteritems():
-#	print key, value
 for key, value in ndt_points.iteritems():
 	print key, value
 
---
+++ validation.py
@@ -0,0 +1,22 @@
+def b():
+	return 1
+	
+def c():
+	return 2
+	
+def initials_check(code):
+	initials=[]
+	initials=code[-2:]
+	return code[:-2]+initials[1]+initials[0]
+	
+def calculate_points(value):
+	j=0
+	test=value
+	for i in value:
+		test[j]= float(i)
+		j+=1
+	return sum(test)/len(test)
+	
+def get_team_name(codes,key):
+	key2=codes[key]
+	return key2[0]
